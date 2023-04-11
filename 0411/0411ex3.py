b76702c843321d80b1722852a761d5821d8857b5
white0467 <127727030+white0467@users.noreply.github.com>
2023-04-11 13:19:35 +0900

    Add files via upload

---
+++ 0411/0411ex3.py
@@ -0,0 +1,14 @@
+num1=int(input('숫자1 :'))
+num2=int(input('숫자2 :'))
+op=input('연산자 :')
+
+if op=="+":
+    print(num1+num2)
+elif op=="-":
+    print(num1-num2)
+elif op=="*":
+    print(num1*num2)
+else:
+    print(num1/num2)
+
+
